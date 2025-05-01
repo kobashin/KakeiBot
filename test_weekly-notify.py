12f230ba89753f5d311fdca8c4082310a437d600
genuine <shin5884genuine@outlook.com>
2025-05-02 00:26:22 +0900

    making lambda-layer

--- test_weekly-notify.py
+++ test_weekly-notify.py
@@ -7,7 +7,7 @@ import requests
 line_token = 'q+XLLASki0d76WWa5qaYf1QUyWBs+6XMT63G0DBRq58EKXsSeUZE9bamsJAruQcla0D9AR2eDqgkbHmm3izaxj27HEgbSP2wKyu7jZrFvzf4GTSRw5KhPLj6oWN7S6EWViM8U2E/LBRkzyHAfCHtbgdB04t89/1O/w1cDnyilFU='
 channel_secret = '860ca8d2ad16e69c2686cc524612c411'
 user_id = 'U55f2c6b5bfc368e349beec673d4dec8a'
-message = 'Test : weekly notify'
+message = "毎週月曜日にメッセージを送ります！\n先週使った金額などをお知らせする予定ですが、実装中です...\nしばらくお待ちください..."
 
 headers = {
     "Authorization": f"Bearer {line_token}",
