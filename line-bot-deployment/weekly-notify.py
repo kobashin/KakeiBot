cf21d3d04addaef5062515ed17b9f56b5f2c6775
kobayashi-laptop <shin5884genuine@outlook.jp>
2025-05-04 18:28:27 +0900

    modified to get deposit of specified week

--- line-bot-deployment/weekly-notify.py
+++ line-bot-deployment/weekly-notify.py
@@ -11,9 +11,59 @@ dynamodb = boto3.client('dynamodb')
 # wait for 3 seconds not to get "ResouurceNotFoundException"
 # the reason is noted in the URL above
 time.sleep(3)
-description = dynamodb.describe_table(TableName='KakeiBot-Table')
-table_size_bytes = description['Table']['TableSizeBytes']
-table_item_count = description['Table']['ItemCount']
+# description = dynamodb.describe_table(TableName='KakeiBot-Table')
+# table_size_bytes = description['Table']['TableSizeBytes']
+# table_item_count = description['Table']['ItemCount']
+
+def get_deposit():
+    response = dynamodb.scan(
+        TableName='KakeiBot-Table',
+        FilterExpression=(
+            '(category = :deposit OR category = :withdrawal) '
+            'AND (#dt BETWEEN :start AND :end)'
+        ),
+        ExpressionAttributeNames={
+            # 'date' is a reserved word, so use ExpressionAttributeNames
+            '#dt': 'date'
+        },
+        ExpressionAttributeValues={
+            ':deposit': {'S': '入金'},
+            ':withdrawal': {'S': '拠出'},
+            ':start': {'S': '2025-0428-0000'},
+            ':end': {'S': '2025-0504-2359'}
+        }
+    )
+
+    # sum up prices for each sub-category
+    """
+        Each item is like that.
+            {
+                'sub-category': {'S': '真一郎'}, 
+                'date': {'S': '2025-0501-1842'}, 
+                'userID': {'S': 'U55f2c6b5b...'}, 
+                'timestamp': {'N': '1746092545035'}, 
+                'category': {'S': '入金'}, 
+                'memo': {'S': '-'}, 
+                'price': {'S': '10000'}
+            }
+    """
+
+    summary = {}
+    # iterate over items
+    for item in response['Items']:
+        sub_category = item['sub-category']['S']
+        price = int(item['price']['S'])
+        # if sub-category is not in summary, add it
+        if sub_category not in summary:
+            summary[sub_category] = 0
+        # add price to sub-category
+        summary[sub_category] += price
+
+    # print summary
+    tmp_deposit = [f"{key}:{value}" for key, value in summary.items()]
+    tmp_deposit = '\n'.join(tmp_deposit)
+    deposit = f"Deposit of last week\n{tmp_deposit}"
+    return deposit
 
 
 def lambda_handler(event, context):
@@ -22,10 +72,13 @@ def lambda_handler(event, context):
     group_id = os.environ['GROUP_ID']  # 送信先ユーザーID
 
     # ここで送信するメッセージ内容を作成（後でDynamoDBから取得したサマリーに置き換える）
+    """
     message = "Test : weekly-notify.py\n" + \
         "DynamoDBのテーブル名はKakeiBot-Tableです。\n" + \
         "KakeiBot-Tableのアイテム数は" + str(table_item_count) + "件です。\n" + \
         "KakeiBot-Tableのサイズは" + str(table_size_bytes) + "バイトです。"
+    """
+    message = get_deposit()
 
     # LINE Messaging API用のヘッダーを設定
     headers = {
